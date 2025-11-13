62601d335de62291e66eb80874222a898d9e4c0c
CaioNas <kn844407@gmail.com>
2025-11-13 00:58:45 -0300

    📦 Adiciona módulo __init__.py do pacote src

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Módulos de processamento e análise de dados
+"""
+
+from .data_processor import DataProcessor
+from .visualizations import Visualizations
+
+__all__ = ['DataProcessor', 'Visualizations']
